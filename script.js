f8d49a8422192db266af445d8b10a9d61102da6a
Gabriel Novaes <148726324+novaesz@users.noreply.github.com>
2023-10-22 23:41:29 -0300

    script.js

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-function encryptBinary(text) {
-    let binaryText = '';
+function encryptCesar(text, shift) {
+    let result = "";
     for (let i = 0; i < text.length; i++) {
-        const charCode = text.charCodeAt(i);
-        const binary = charCode.toString(2); // Converte o caractere para binário
-        binaryText += binary + ' '; // Adiciona espaço para separar os caracteres binários
+        let char = text[i];
+        if (char.match(/[a-z]/i)) {
+            let code = text.charCodeAt(i);
+            if (char === char.toLowerCase()) {
+                char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+            } else {
+                char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
+            }
+        }
+        result += char;
     }
-    return binaryText;
+    return result;
 }
 
-function decryptBinary(binaryText) {
-    const binaryArray = binaryText.trim().split(' '); // Divide os caracteres binários
-    let decryptedText = '';
-    for (const binary of binaryArray) {
-        const charCode = parseInt(binary, 2); // Converte o binário de volta para o código de caractere
-        decryptedText += String.fromCharCode(charCode);
-    }
-    return decryptedText;
+function decryptCesar(text, shift) {
+    return encryptCesar(text, 26 - shift);
 }
 
 document.getElementById("encryptButton").addEventListener("click", function () {
     const plaintext = document.getElementById("plaintext").value;
-    const encryptedText = encryptBinary(plaintext);
+    const shift = 3; // O valor de deslocamento da cifra de César
+    const encryptedText = encryptCesar(plaintext, shift);
     document.getElementById("encryptedText").textContent = encryptedText;
 });
 
 document.getElementById("decryptButton").addEventListener("click", function () {
     const ciphertext = document.getElementById("ciphertext").value;
-    const decryptedText = decryptBinary(ciphertext);
+    const shift = 3; // O mesmo valor de deslocamento usado para criptografar
+    const decryptedText = decryptCesar(ciphertext, shift);
     document.getElementById("decryptedText").textContent = decryptedText;
 });
